ba4ec1130fdfbe8dfdb951c843a7cb620e461c59
oyr <1364782109@qq.com>
2018-12-06 22:57:40 +0800

    我是小明我修改了文件！！！！

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,6 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("my is xiaoming update ");
     }
 
 }
