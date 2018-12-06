452f7dd288a9dff682220a8fab27c69f84511f41
oyr <1364782109@qq.com>
2018-12-06 18:08:24 +0800

    初始化。

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName Application
+ * @Description TODO
+ * @Author ouyang
+ * @Date 2018/12/6 17:42
+ * @Version 1.0
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
---
+++ src/main/java/GitDemo.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName GitDemo
+ * @Description TODO
+ * @Author ouyang
+ * @Date 2018/12/6 17:52
+ * @Version 1.0
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+        System.out.println("idea 操作 git，开心！！！");
+    }
+
+}
