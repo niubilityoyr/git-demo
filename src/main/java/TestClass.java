2df9f8a8be9c31df38e07fa47d05781ba48e2998
oyr <1364782109@qq.com>
2018-12-10 16:16:46 +0800

    新建一个test分支。

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName TestClass
+ * @Description TODO
+ * @Author ouyang
+ * @Date 2018/12/10 16:16
+ * @Version 1.0
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("my is test branch");
+    }
+
+}
