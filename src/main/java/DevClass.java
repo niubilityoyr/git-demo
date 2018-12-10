cb4f564c5070fe7052b93bb0bfea0d44da7395e0
oyr <1364782109@qq.com>
2018-12-10 16:27:18 +0800

    新建devclass文件

---
+++ src/main/java/DevClass.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName DevClass
+ * @Description TODO
+ * @Author ouyang
+ * @Date 2018/12/10 16:26
+ * @Version 1.0
+ */
+public class DevClass {
+    public static void main(String[] args) {
+        System.out.println("my is dev branch");
+    }
+}
